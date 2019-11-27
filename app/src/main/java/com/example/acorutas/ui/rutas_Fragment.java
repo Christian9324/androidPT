cb4d43304fc21a48e0ed94bd034580904aac2ed5
Christian <c3_fireblade@hotmail.com>
2019-11-26 19:13:15 -0600

    v0.4 de rutas
    se creo la union con el web service y se agrego el spinner con 5b elementos

--- app/src/main/java/com/example/acorutas/ui/rutas_Fragment.java
+++ app/src/main/java/com/example/acorutas/ui/rutas_Fragment.java
@@ -18,17 +18,26 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.acorutas.Data.databases.adminBDDhelper;
+import com.example.acorutas.Data.models.Ruta;
+import com.example.acorutas.Data.remote.ApiUtils;
+import com.example.acorutas.Data.remote.DjangoRestApi;
 import com.example.acorutas.R;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.example.acorutas.Data.databases.estaciones.estacionesMetro;
 
@@ -43,6 +52,9 @@ public class rutas_Fragment extends Fragment {
     private TextView tv_consulta;
     private AutoCompleteTextView estDes;
     private Spinner estOrig;
+    private Button cal_ruta;
+
+    private DjangoRestApi djangoRestApi;
 
     private WebView Wmapa;
 
@@ -72,8 +84,18 @@ public class rutas_Fragment extends Fragment {
 
         estDes = (AutoCompleteTextView) v.findViewById(R.id.ACTV_estacionDestino);
         estOrig = (Spinner) v.findViewById(R.id.Spinner_estacionInicio);
+        cal_ruta = (Button) v.findViewById(R.id.btn_ruta);
+
+        djangoRestApi = ApiUtils.getAPIService();
 
         ACTV_datosIniciales();
+
+        cal_ruta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WS_calcularRuta();
+            }
+        });
 
         return v;
     }
@@ -137,10 +159,12 @@ public class rutas_Fragment extends Fragment {
         // numero de estacion
         //Log.d("dato mejor" , String.valueOf(tresM[0][0]));
 
-        String[] datos_spinner = new String[3];
+        String[] datos_spinner = new String[5];
         datos_spinner[0] = estacionesMetro[(int)tresM[0][0] - 1][2];
         datos_spinner[1] = estacionesMetro[(int)tresM[1][0] - 1][2];
         datos_spinner[2] = estacionesMetro[(int)tresM[2][0] - 1][2];
+        datos_spinner[3] = estacionesMetro[(int)tresM[3][0] - 1][2];
+        datos_spinner[4] = estacionesMetro[(int)tresM[4][0] - 1][2];
 
         estOrig.setAdapter(new ArrayAdapter<String>
                 (getActivity().getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, datos_spinner));
@@ -153,11 +177,13 @@ public class rutas_Fragment extends Fragment {
 
 
 
+
+
     public double[][] mejores(double latitud, double longitud)
     {
 
         double[][] lista;
-        double[][] mejores = new double[3][2];
+        double[][] mejores = new double[5][3];
 
         lista = distancia(latitud, longitud);
 
@@ -175,9 +201,20 @@ public class rutas_Fragment extends Fragment {
 
         Log.i("datos ", String.valueOf(listaC[0][1]));
          */
+
+        /*
+        for (int f = 0; f < 195; f++){
+            Log.i("estaciones", String.valueOf(lista[f][2]));
+        }
+         */
+
+
         mejores[0] = lista[1];
         mejores[1] = lista[2];
         mejores[2] = lista[3];
+        mejores[3] = lista[4];
+        mejores[4] = lista[5];
+
 
         return mejores;
 
@@ -186,18 +223,21 @@ public class rutas_Fragment extends Fragment {
 
     public double[][] distancia(double latitud, double longitud){
 
-        double[][] distancia = new double[196][2];
+        double[][] distancia = new double[196][3];
         int indice;
         double la;
         double lo;
+        double indice_rep;
 
         for ( String[] dato : estacionesMetro){
             indice = Integer.parseInt(dato[0]);
             la = Double.parseDouble(dato[3]);
             lo = Double.parseDouble(dato[4]);
+            indice_rep = Double.parseDouble(dato[5]);
 
             distancia[indice][0] = indice;
             distancia[indice][1] = Math.sqrt(Math.pow(latitud-la ,2) + Math.pow(longitud-lo ,2));
+            distancia[indice][2] = indice_rep;
         }
 
         return distancia;
@@ -209,6 +249,7 @@ public class rutas_Fragment extends Fragment {
         double auxiliar0;
         double auxiliar1;
         double[][] arregloO;
+        double auxiliar3;
 
         for(int i = 2; i < 196; i++)
         {
@@ -218,17 +259,83 @@ public class rutas_Fragment extends Fragment {
                 {
                     auxiliar0 = arreglo[j][0];
                     auxiliar1 = arreglo[j][1];
+                    auxiliar3 = arreglo[j][2];
 
                     arreglo[j][0] = arreglo[j+1][0];
                     arreglo[j][1] = arreglo[j+1][1];
+                    arreglo[j][2] = arreglo[j+1][2];
                     arreglo[j+1][0] = auxiliar1;
                     arreglo[j+1][1] = auxiliar0;
+                    arreglo[j+1][2] = auxiliar3;
                 }
             }
         }
 
         arregloO = arreglo;
         return arregloO;
+    }
+
+    public void WS_calcularRuta(){
+
+        String i,d;
+        int ini=0, des=0;
+
+        i = estOrig.getSelectedItem().toString();
+        d = estDes.getText().toString();
+
+
+        for (int ill = 0; ill < 195; ill++){
+            if(estacionesMetro[ill][2].equals(i)){
+                ini = Integer.parseInt(estacionesMetro[ill][5]);
+            }
+
+            if(estacionesMetro[ill][2].equals(d)){
+                des = Integer.parseInt(estacionesMetro[ill][5]);
+            }
+        }
+
+        Log.i("ini", i);
+        Log.i("des", d);
+
+        crearRuta(ini, des);
+
+    }
+
+    private void crearRuta(int ini, int dest){
+
+        Ruta ruta_nueva = new Ruta(ini, dest);
+
+        Call<Ruta> call = djangoRestApi.crearRuta(ruta_nueva);
+
+        call.enqueue(new Callback<Ruta>() {
+            @Override
+            public void onResponse(Call<Ruta> call, Response<Ruta> response) {
+
+                if(!response.isSuccessful()){
+                    Toast.makeText(getActivity().getApplicationContext(), "Code: " + response.code(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Ruta rutas = response.body();
+
+                String contenido = "";
+                contenido += "Code: " + response.code() + "\n";
+                contenido += "Estacion de Inicio: " + rutas.getEstacionInicio() + "\n";
+                contenido += "Estacion Destino: " + rutas.getEstacionDestino() + "\n";
+                contenido += "Ruta establecida " + rutas.getRutaNodos() + "\n\n";
+
+                tv_consulta.append(contenido);
+
+            }
+
+            @Override
+            public void onFailure(Call<Ruta> call, Throwable t) {
+
+                Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 
 }
