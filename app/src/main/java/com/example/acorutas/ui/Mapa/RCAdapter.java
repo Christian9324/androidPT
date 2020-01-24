63e8f3d0c6fe74b53d5bafadef38f592f18dc46d
Christian <c3_fireblade@hotmail.com>
2020-01-23 20:21:06 -0600

    v0.11 RecyclerView basico
    Se corrigio el problema del recyclerView

--- app/src/main/java/com/example/acorutas/ui/Mapa/RCAdapter.java
+++ app/src/main/java/com/example/acorutas/ui/Mapa/RCAdapter.java
@@ -1,33 +1,57 @@
 package com.example.acorutas.ui.Mapa;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.acorutas.Data.models.estacionInformacion;
+import com.example.acorutas.R;
+
+import java.util.ArrayList;
+
 public class RCAdapter extends RecyclerView.Adapter<RCAdapter.ViewHolderDatos> {
+
+    ArrayList<estacionInformacion> listaDatos;
+
+    public RCAdapter(ArrayList<estacionInformacion> listaDatos) {
+        this.listaDatos = listaDatos;
+    }
+
     @NonNull
     @Override
     public RCAdapter.ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_estacion, null, false);
+        return new ViewHolderDatos(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RCAdapter.ViewHolderDatos holder, int position) {
-
+        holder.asignarDatos(listaDatos.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaDatos.size();
     }
 
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
 
+        TextView tv_estacionInfo;
+        ImageView imEstacion;
 
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
+            tv_estacionInfo = (TextView) itemView.findViewById(R.id.TV_estacionInfo);
+            imEstacion = (ImageView) itemView.findViewById(R.id.image_estacion);
+        }
+
+        public void asignarDatos(estacionInformacion estacionInformacion) {
+            tv_estacionInfo.setText(estacionInformacion.getInformacion());
         }
     }
 }
